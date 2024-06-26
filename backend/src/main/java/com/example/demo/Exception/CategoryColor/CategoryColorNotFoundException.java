1a13603611d062a15d957f7a810c298b1ea00ff6
nourhan <nourhansaeed707@gmail.com>
2024-06-27 00:25:36 +0300

    add categoryColor and categorySize resources

---
+++ backend/src/main/java/com/example/demo/Exception/CategoryColor/CategoryColorNotFoundException.java
@@ -0,0 +1,2 @@
+package com.example.demo.Exception.CategoryColor;public class CategoryColorNotFoundException {
+}
---
+++ backend/src/main/java/com/example/demo/Exception/CategorySize/CategorySizeNotFoundException.java
@@ -0,0 +1,2 @@
+package com.example.demo.Exception.CategorySize;public class CategorySizeNotFoundException {
+}
