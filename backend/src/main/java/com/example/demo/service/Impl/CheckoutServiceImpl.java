05f42c08e3225f1cd729c431dd8d0f84295e2a98
nourhan <nourhansaeed707@gmail.com>
2024-10-12 21:56:33 +0300

    checkout functions

--- backend/src/main/java/com/example/demo/service/Impl/CheckoutServiceImpl.java
+++ backend/src/main/java/com/example/demo/service/Impl/CheckoutServiceImpl.java
@@ -1,11 +1,20 @@
 package com.example.demo.service.Impl;
+import com.example.demo.entity.CreditCardInfo;
+import com.example.demo.entity.Orders;
+import com.example.demo.entity.ShippingInfo;
 import com.example.demo.model.CheckoutDTO;
+import com.example.demo.model.CreditCardInfoDTO;
+import com.example.demo.model.OrdersDTO;
+import com.example.demo.model.ShippingInfoDTO;
 import com.example.demo.repository.CreditCardInfoRepository;
 import com.example.demo.repository.OrdersRepository;
 import com.example.demo.repository.ShippingInfoRepository;
 import com.example.demo.service.CheckoutService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CheckoutServiceImpl implements CheckoutService {
     @Autowired
     private ShippingInfoRepository shippingInfoRepository;
@@ -13,9 +22,32 @@ public class CheckoutServiceImpl implements CheckoutService {
     private CreditCardInfoRepository creditCardRepository;
     @Autowired
     private OrdersRepository ordersRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public CheckoutDTO processCheckout(CheckoutDTO checkoutDTO) {
-        return null;
+        // save shipping info
+        ShippingInfo shippingInfo = saveShippingInfo(checkoutDTO.getShippingInfoDTO());
+        CreditCardInfo creditCardInfo = saveCreditCardInfo(checkoutDTO.getCreditCardInfoDTO());
+        saveOrder(checkoutDTO.getOrdersDTO(), shippingInfo, creditCardInfo);
+        return checkoutDTO;
+    }
+
+    private ShippingInfo saveShippingInfo(ShippingInfoDTO shippingInfoDTO) {
+        ShippingInfo shippingInfo = modelMapper.map(shippingInfoDTO, ShippingInfo.class);
+        return shippingInfoRepository.save(shippingInfo);
+    }
+
+    private CreditCardInfo saveCreditCardInfo(CreditCardInfoDTO creditCardInfoDTO) {
+        CreditCardInfo creditCardInfo = modelMapper.map(creditCardInfoDTO, CreditCardInfo.class);
+        return creditCardRepository.save(creditCardInfo);
+    }
+
+    private Orders saveOrder(OrdersDTO ordersDTO, ShippingInfo shippingInfo , CreditCardInfo creditCardInfo) {
+        Orders order = modelMapper.map(ordersDTO, Orders.class);
+        order.setShippingInfo(shippingInfo);
+        order.setCreditCardInfo(creditCardInfo);
+        return ordersRepository.save(order);
     }
 }
