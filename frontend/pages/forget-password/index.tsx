6eb5a3be4f55177442f154725ddb9100a8d57b56
nourhan <nourhansaeed707@gmail.com>
2024-11-10 00:43:20 +0200

    change style

--- frontend/pages/forget-password/index.tsx
+++ frontend/pages/forget-password/index.tsx
@@ -7,11 +7,21 @@ function ForgetPasswordPage() {
   const { setUserDataForEmail, setEmail } = useSendEmailForget();
 
   useEffect(() => {
-    console.log("insiiiiiide send email forget password");
     setUserDataForEmail({email: email});
   }, [email, setUserDataForEmail]);
 
-  return <div>We will send you message so you can change you password</div>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white shadow-lg rounded-lg p-6 max-w-md text-center">
+        <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+          Password Reset
+        </h2>
+        <p className="text-gray-600">
+          We will send you an email with instructions to reset your password.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ForgetPasswordPage;
